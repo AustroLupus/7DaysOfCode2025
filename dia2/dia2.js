6b4d41e64cab8171eb6c9e9c17957def5d68bfe5
AustroLupus <89206747+AustroLupus@users.noreply.github.com>
2025-02-12 09:13:38 -0300

    Optional second version using switch case

--- dia2/dia2.js
+++ dia2/dia2.js
@@ -3,7 +3,7 @@ let edad = prompt("¿Cuántos años tienes?");
 let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
 alert("Hola " + nombre +", tienes "+ edad + " años y ya estás aprendiendo " + lenguaje +"!")
 
-
+/*
 let gustoOk = false
 while (!gustoOk) {
     let gusto = prompt("¿Te gusta estudiar " + lenguaje + "? Responde con el número 1 para SÍ o 2 para NO.")
@@ -15,5 +15,19 @@ while (!gustoOk) {
         gustoOk = true;
     } else {
         alert("Debe ingresar 1 o 2")
+    }
+}
+*/
+
+while(true) {
+    switch (prompt("¿Te gusta estudiar " + lenguaje + "? Responde con el número 1 para SÍ o 2 para NO.")) {
+        case "1":
+            alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
+            break;
+        case "2":
+            alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
+            break;
+        default:
+            alert("Debe ingresar 1 o 2")
     }
 }
