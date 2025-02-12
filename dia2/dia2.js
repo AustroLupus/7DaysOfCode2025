c13454fcd6245ee80503882ebbad82fe255f8ac6
AustroLupus <89206747+AustroLupus@users.noreply.github.com>
2025-02-12 09:12:15 -0300

    First requirement completed

--- dia2/dia2.js
+++ dia2/dia2.js
@@ -1,3 +1,4 @@
-let nombre = prompt("Nombre");
-let edad = prompt("Edad");
-let lenguaje = prompt("Lenguaje");
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
+alert("Hola " + nombre +", tienes "+ edad + " años y ya estás aprendiendo " + lenguaje +"!")
