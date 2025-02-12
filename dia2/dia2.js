66f81a2513d1b0c763f1ceacc48b3dc6a400888a
AustroLupus <89206747+AustroLupus@users.noreply.github.com>
2025-02-12 09:12:44 -0300

    Optional first version using if statement

--- dia2/dia2.js
+++ dia2/dia2.js
@@ -2,3 +2,18 @@ let nombre = prompt("¿Cuál es tu nombre?");
 let edad = prompt("¿Cuántos años tienes?");
 let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
 alert("Hola " + nombre +", tienes "+ edad + " años y ya estás aprendiendo " + lenguaje +"!")
+
+
+let gustoOk = false
+while (!gustoOk) {
+    let gusto = prompt("¿Te gusta estudiar " + lenguaje + "? Responde con el número 1 para SÍ o 2 para NO.")
+    if (gusto === "1") {
+        alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
+        gustoOk = true;
+    } else if (gusto === "2") {
+        alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
+        gustoOk = true;
+    } else {
+        alert("Debe ingresar 1 o 2")
+    }
+}
